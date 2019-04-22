39395b603d686d75e176a6fbb1df4527e3d159e7
anagalakshmi.ip <anagalakshmi.ip@gmail.com>
2019-04-22 03:37:10 -0700

    javascript binding

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function clickMe() { 
-    var x = document.createElement("p"); 
-    var t = document.createTextNode("hai sai how are you"); 
-    x.appendChild(t); 
-    document.body.appendChild(x); 
-} 
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json => {console.log(json)
+    var table=document.getElementById("avvaru")  
+    for (var i=0; i<json.length; i++){
+        var tr =document.createElement("tr");
+        var td =document.createElement("td");
+    td.innerHTML=json[i]["id"]; 
+    var td2 =document.createElement("td");
+    td2.innerHTML=json[i]["title"];
+    tr.appendChild(td);
+    tr.appendChild(td2);
+        table.appendChild(tr);
+       // document.writeln(json[i]["userId"])
+    }
+})
